99d7f945468364659bae92f84a430434856c324e
boris-dsh <borisdejong12@icloud.com>
2025-02-21 17:24:45 +0100

    new stuff

--- reflectance/spp_dispersion_3.py
+++ reflectance/spp_dispersion_3.py
@@ -21,8 +21,8 @@ gamma = 0.05 * e  # coupling energy [J]
 gamma_eV = gamma / e # coupling energy [eV]
 p = 600e-9  # pitch size of grating [m]
 g = 2 * np.pi / p  # reciprocal wave vector [rad/m]
-L = 5e-6 # grating size [m]
-phi = np.pi / 2  # phase shift
+L = 50e-6 # grating size [m]
+phi = np.pi / 3  # phase shift
 x_vals = np.linspace(-L/2, L/2, 2000) # spatial width of grating 
 h_prime_x = -g * np.sin(g * x_vals + phi) # assume cosine form of first harmonic, so derivative -> -sin
 
@@ -50,7 +50,6 @@ E_vals = (hbar * omega) / e  # energy in [eV]
 
 """...........................FUNCTIONS.........................."""
 
-
 def compute_eig(k: float) -> tuple: 
     """
     Computes the eigenvalues and eigenvectors of the coupling matrices for a given wavevector k.
@@ -63,10 +62,8 @@ def compute_eig(k: float) -> tuple:
 
     Returns:
         tuple: 
-            - eigvals1 (np.ndarray): Eigenvalues of the first coupling matrix (energy levels in eV).
-            - eigvecs1 (np.ndarray): Corresponding eigenvectors of the first coupling matrix.
-            - eigvals2 (np.ndarray): Eigenvalues of the second coupling matrix (energy levels in eV).
-            - eigvecs2 (np.ndarray): Corresponding eigenvectors of the second coupling matrix.
+            - eigvals (np.ndarray): Eigenvalues of the coupling matrix (energy levels in eV).
+            - eigvecs (np.ndarray): Corresponding eigenvectors of the coupling matrix.
     """
     eigvals, eigvecs = eig(np.array([
         [E_of_k_right(k), gamma_eV],
@@ -91,6 +88,44 @@ def gaussian_kernel(size: int, sigma: float) -> np.ndarray:
 
     return gauss / gauss.sum()
 
+def make_mesh(arr: np.ndarray, wavelengths: np.ndarray) -> tuple:
+    """
+    Create a mesh of [angle, wavelength] coordinates. The angle grid can be converted to k_x.
+    Assuming NA = 0.8, n = 1.0
+
+    Parameters:
+        arr (np.ndarray): 2D array of intensity values (shape: [num_wavelengths, num_angles]).
+        wavelengths (np.ndarray): Array of wavelengths (shape: [num_wavelengths]).
+
+    Returns:
+        tuple:
+            - np.ndarray: points
+            - np.ndarray: k_x
+            - np.ndarray: wavelengths
+    """
+
+    max_angle = np.arcsin(0.8 / 1) * (180 / np.pi)  # NA = 0.8, n = 1.0 for air
+    angles = np.linspace(-max_angle, max_angle, arr.shape[1])
+    wavelength_grid, angle_grid = np.meshgrid(wavelengths, angles, indexing='ij')
+    k_x = (2 * np.pi * np.sin(np.radians(angle_grid))) / (wavelength_grid * 1e-3) # angle -> k_x
+    points =  arr.flatten()
+
+    return points, k_x, wavelength_grid
+
+def wavelength_to_energy(wavelength_nm: np.ndarray)-> np.ndarray:
+    """Convert wavelength in nm to energy in eV."""
+    wavelength_m = wavelength_nm * 1e-9
+    energy_J = (h * c) / wavelength_m
+    energy_eV = energy_J / e
+    return energy_eV
+
+def energy_to_wavelength(energy_eV: np.ndarray)-> np.ndarray:
+    """Convert energy in eV to wavelength in nm."""
+    energy_J = energy_eV * e
+    wavelength_m = (h * c) / energy_J
+    wavelength_nm = wavelength_m * 1e9
+    return wavelength_nm
+
 """.........................CALCULATION........................"""
 
 # initialize interpolation functions E(k)
@@ -102,7 +137,7 @@ k_vals = np.linspace(-7e6, 7e6, 1000)
 
 # plt.plot(k_vals, E_of_k_right(k_vals))
 # plt.plot(k_vals, E_of_k_left(k_vals))
-# plt.show()
+# plt.show(); plt.close() # plot E(k) interpolations
 
 evector_arr =  np.zeros(shape=k_vals.shape, dtype=object); eval_arr = np.zeros(shape=k_vals.shape, dtype=object)
 
@@ -111,7 +146,106 @@ for i, k in enumerate(k_vals):
     evector_arr[i] = evec
     eval_arr[i] = eval
 
-eigenvectors_separated = [[evec[:, j] for j in range(evec.shape[1])] for evec in evector_arr]
+eigenvectors = [[evec[:, j] for j in range(evec.shape[1])] for evec in evector_arr]
+eigenvalues = [[eval[j] for j in range(eval.shape[0])] for eval in eval_arr]
+
+for j in range(evec.shape[1]):
+    real_parts = [evec[j] for evec in eigenvectors]
+
+    plt.figure()
+    plt.plot(k_vals, np.abs(real_parts), label=f'eigenvector {j+1}')
+    plt.xlabel(r'$k$ [m$^{-1}$]')
+    plt.ylabel('eigenvector component')
+    plt.legend()
+    plt.show(); plt.close() # plot eigenvectors
+
+# for l in range(eval.shape[0]):
+#     plt.figure()
+#     plt.plot(k_vals, [eval[l] for eval in eigenvalues], 'o', label=f'eigenvalue {l+1}')
+#     plt.xlabel(r'$k$ [m$^{-1}$]')
+#     plt.ylabel('eigenvalue [eV]')
+#     plt.legend()
+#     plt.show(); plt.close() # plot eigenvalues
+
+count = 0
+energy_vals = np.linspace(1.8, 3, 1000)
+intensity_map = np.zeros((len(energy_vals), len(k_vals)))
+for i, k in enumerate(k_vals):
+
+    eigval1 = eval_arr[i][0]
+    eigval2 = eval_arr[i][1]
+
+    electric_field_1 = (evector_arr[i][0, 0]) * (np.exp(1j * (k + n * g) * x_vals)) - (evector_arr[i][0, 1]) * (np.exp(1j * (k - n * g) * x_vals))
+    electric_field_2 = (evector_arr[i][1, 0]) * (np.exp(1j * (k + n * g) * x_vals)) - (evector_arr[i][1, 1]) * (np.exp(1j * (k - n * g) * x_vals))
+
+    # if count % 400 == 0:
+    #     plt.figure()
+    #     plt.plot(x_vals, h_prime_x * electric_field_1, label='electric field 1')
+    #     plt.plot(x_vals, h_prime_x * electric_field_2, label='electric field 2')
+    #     plt.xlabel(r'$x$ [m]')
+    #     plt.ylabel('electric field')
+    #     plt.legend()
+    #     plt.show(); plt.close() # plot electric fields
+    
+
+    M1 = np.trapezoid(h_prime_x * electric_field_1 * np.exp(-1j * k * x_vals), x_vals) # num integrate
+    M2 = np.trapezoid(h_prime_x * electric_field_2 * np.exp(-1j * k * x_vals), x_vals)
+
+    E_idx1 = np.abs(energy_vals - eigval1).argmin()
+    intensity_map[E_idx1, i] += np.abs(M1)**2 * p / L
+    E_idx2 = np.abs(energy_vals - eigval2).argmin()
+    intensity_map[E_idx2, i] += np.abs(M2)**2 * p / L
+
+    count += 1
 
 
-print(eigenvectors_separated[0:, 0])
+intensity_map[:, [0, -1]] = 0 # remove edge intensity for convolution
+intensity_map[[0, -1], :] = 0
+
+# sinc_kernel = np.sinc(k_vals * L / 2) ** 2
+# sinc_kernel /= np.sum(sinc_kernel)
+
+# intensity_map = np.array([
+#     fftconvolve(intensity_map[i], sinc_kernel, mode='same')
+#     for i in range(intensity_map.shape[0])
+# ])
+# kernel = gaussian_kernel(50, 5)[:, None] * gaussian_kernel(50, 5)
+# intensity_map = fftconvolve(intensity_map, kernel, mode="same")
+# intensity_map /= np.max(intensity_map)
+
+plt.plot(k_vals, np.sum(intensity_map, axis=0))
+plt.show(); plt.close()
+
+plt.imshow(intensity_map, extent=[k_vals[0], k_vals[-1], energy_vals[0], energy_vals[-1]], 
+           origin='lower', aspect='auto', cmap='inferno')
+plt.colorbar(label="intensity")
+plt.show()
+
+
+energy_min, energy_max = energy_to_wavelength(np.array([700, 400]))
+E_vals = np.linspace(energy_min, energy_max, 1000)  # energy [eV]
+wavelengths = energy_to_wavelength(E_vals)  # eV -> [nm]
+intensity_flat, k_x, wavelength_grid = make_mesh(intensity_map, wavelengths)
+energy_grid = wavelength_to_energy(wavelength_grid)  # convert nm to [eV] 
+
+plt.figure(figsize=(4, 6))
+ax1 = plt.gca()
+pcm = ax1.pcolormesh(k_x, energy_grid, intensity_map, cmap='inferno', shading='auto')
+
+plt.colorbar(pcm, ax=ax1, label='intensity (norm.)', location='top')
+ax1.set_xlabel(r'$k_x$ (rad/$\mu$m)')
+ax1.set_ylabel(r'Energy (eV)')
+
+ax2 = ax1.twinx()
+ymin, ymax = ax1.get_ylim()
+ax2.set_ylim(ymin, ymax)
+
+custom_wavelength_ticks = np.array([400, 500, 600, 700])
+custom_energy_ticks = wavelength_to_energy(custom_wavelength_ticks)
+
+ax2.set_yticks(custom_energy_ticks)
+ax2.set_yticklabels([f'{w}' for w in custom_wavelength_ticks])
+ax2.set_ylabel('wavelength (nm)')
+
+plt.tight_layout()
+plt.show()
---
+++ reflectance/spp_dispersion_4.py
@@ -0,0 +1,157 @@
+"""
+Author: Boris de Jong
+"""
+
+
+"""...........................PACKAGES..........................."""
+
+import numpy as np, matplotlib.pyplot as plt, os, pandas as pd
+from scipy.interpolate import interp1d
+from numpy.linalg import eig
+from scipy.signal import fftconvolve
+dir = os.path.dirname(__file__)
+
+"""...........................CONSTANTS.........................."""
+
+c = 2.998e8  # light speed [m/s]
+h = 6.62607015e-34  # planck's constant [Js]
+hbar = h / (2 * np.pi)  # reduced planck's constant [Js]
+e = 1.602176634e-19  # elementary charge [C]
+gamma = 0.05 * e  # coupling energy [J]
+gamma_eV = gamma / e # coupling energy [eV]
+p = 600e-9  # pitch size of grating [m]
+g = 2 * np.pi / p  # reciprocal wave vector [rad/m]
+L = 5e-6 # grating size [m]
+phi = np.pi / 2  # phase shift
+x_vals = np.linspace(-L/2, L/2, 2000) # spatial width of grating 
+h_prime_x = -g * np.sin(g * x_vals + phi) # assume cosine form of first harmonic, so derivative -> -sin
+plasma_freq = 9.01e15 # plasma frequency of silver [Hz]
+collision_freq = 1.38e14 # collision frequency of silver [Hz]
+
+"""..........................FUNCTIONS........................."""
+
+def n_eff_func(omega: float) -> complex:
+    """
+    Computes the effective index for a given angular frequency omega.
+    """
+    eps_m = 1 - (plasma_freq**2) / (omega**2 + 1j * collision_freq * omega)  # metal permittivity
+    eps_d = 1.00  # vacuum permittivity
+    return np.sqrt((eps_d * eps_m) / (eps_d + eps_m))  # effective index
+
+def k_spp(k: list) -> np.ndarray:
+    """
+    Computes the SPP wave number for a given angular wave number k0.
+    """
+    k = np.asarray(k)
+    omega = c * k  # angular frequency [rad/s]
+    neff = n_eff_func(omega)
+    return k * neff  # SPP wave number [rad/m]
+
+def k_spp_func(omega: float) -> float:
+    """
+    Computes the SPP wave number for a given angular frequency omega.
+    """
+    n_eff = n_eff_func(omega)
+    return (omega / c) * n_eff
+
+def e_field(k: float) -> np.ndarray:
+    """
+    Computes the electric field for a given angular wave number k.
+    """
+    if k == 0:
+        kspp = 0
+        n_eff = 1
+    else:
+        kspp = k_spp(k)
+        omega = c * k  # angular frequency [rad/s]
+        n_eff = n_eff_func(omega)  # effective index
+    norm_func_plus = (np.sqrt((hbar * c * kspp)**2 + (gamma * n_eff)**2) + (hbar * c * kspp)) / (gamma * n_eff)
+    norm_func_minus = (np.sqrt((hbar * c * kspp)**2 + (gamma * n_eff)**2) - (hbar * c * kspp)) / (gamma * n_eff)
+
+    e_field_plus = 1/np.sqrt(1 + norm_func_plus**2) * (np.exp(1j * (kspp + g) * x_vals) + norm_func_plus * np.exp(1j * (kspp - g) * x_vals))  # electric field +
+    e_field_minus = 1/np.sqrt(1 + norm_func_minus**2) * (np.exp(1j * (kspp + g) * x_vals) - norm_func_minus * np.exp(1j * (kspp - g) * x_vals))  # electric field -
+    return e_field_plus, e_field_minus # electric fields
+
+def energy_kspp_interpolation():
+    """
+    Creates an interpolation function for energy as a function of k_spp.
+    """
+    omega_vals = np.linspace(1e5 * c, 1e7 * c, 1000)  # range of angular frequencies
+    k_spp_vals = np.array([k_spp_func(omega).real for omega in omega_vals])
+    energy_vals = (hbar * omega_vals / e).real  # energy in [eV]
+
+    return interp1d(k_spp_vals-g, energy_vals, kind='linear', fill_value="extrapolate"), interp1d(-k_spp_vals+g, energy_vals, kind='linear', fill_value="extrapolate")
+
+def eigenvalues(k: float) -> np.ndarray:
+    """
+    Computes the eigenvalues of the coupling matrices for a given wavevector k.
+    """
+    A = np.array([[energy_from_kspp_minus(k), gamma_eV], [gamma_eV, energy_from_kspp_plus(k)]])
+    return np.linalg.eigvals(A)  # eigenvalues
+
+"""............................MAIN..........................."""
+
+energy_from_kspp_minus, energy_from_kspp_plus = energy_kspp_interpolation()
+
+kv = np.linspace(-1e7, 1e7, 1000)
+energy_values = energy_from_kspp_minus(kv)
+
+plt.plot(kv, energy_from_kspp_minus(kv), label='E(kspp-)')
+plt.plot(kv, energy_from_kspp_plus(kv), label='E(kspp)+')
+plt.xlabel('k_spp [rad/m]')
+plt.ylabel('energy [eV]')
+plt.legend()
+plt.show(); plt.close()
+
+eigenvalues_array = np.array([eigenvalues(k) for k in kv])
+plt.plot(kv, eigenvalues_array[:, 0], 'o', label='E1')
+plt.plot(kv, eigenvalues_array[:, 1], 'o', label='E2')
+plt.xlabel('k [rad/m]')
+plt.ylabel('eigenvalues [eV]')
+plt.legend()
+plt.show(); plt.close()
+
+plt.plot(x_vals, (e_field(0)[0]), label='E+') # at k0 = 0: ~cos(gx)
+plt.plot(x_vals, np.imag(e_field(0)[1]), label='E-') # at k0 = 0: ~sin(gx)
+plt.legend()
+plt.show(); plt.close()
+
+intensity_plus = []
+intensity_minus = []
+
+for i in range(len(kv)):
+    intensity_plus.append(np.trapezoid(h_prime_x * (e_field(kv[i])[0]) * np.exp(1j * kv[i] * x_vals), x_vals))
+    intensity_minus.append(np.trapezoid(h_prime_x * ((e_field(kv[i])[1])) * np.exp(1j * kv[i] * x_vals), x_vals))
+#     # if i % 50 == 0:
+#     #     plt.plot(x_vals, h_prime_x * (e_field(kv[i])[0]) * np.exp(1j * kv[i] * x_vals), label='h\'(x) * E+')
+#     #     plt.plot(x_vals, h_prime_x * (np.imag(e_field(kv[i])[1])) * np.exp(1j * kv[i] * x_vals), label='h\'(x) * E-')
+#     #     plt.legend()
+#     #     plt.show(); plt.close()
+
+plt.plot(kv, np.abs(intensity_plus)**2, label='int +')
+plt.plot(kv, np.abs(intensity_minus)**2, label='int -')
+plt.legend()
+plt.show(); plt.close()
+
+energy_vals = np.linspace(energy_from_kspp_minus(-1e7), energy_from_kspp_minus(1e7), 1000)
+intensity_grid = np.zeros((len(energy_vals), len(kv)))
+
+for i, k in enumerate(kv):
+    energy_minus = energy_from_kspp_minus(k)
+    energy_plus = energy_from_kspp_plus(k)
+    energy_idx = np.abs(energy_vals - eigenvalues(k)[0]).argmin()
+    intensity_grid[energy_idx, i] = np.abs(intensity_plus[i])**2
+    energy_idx = np.abs(energy_vals - eigenvalues(k)[1]).argmin()
+    intensity_grid[energy_idx, i] = np.abs(intensity_minus[i])**2
+
+plt.imshow(intensity_grid, extent=[kv[0], kv[-1], energy_vals[0], energy_vals[-1]], aspect='auto', origin='lower', cmap='plasma')
+plt.colorbar(label='intensity')
+plt.xlabel('k [rad/m]')
+plt.ylabel('energy [eV]')
+plt.show(); plt.close()
+
+
+plt.plot(kv, intensity_grid.sum(axis=0))
+plt.xlabel('k [rad/m]')
+plt.ylabel('intensity')
+plt.show(); plt.close()
